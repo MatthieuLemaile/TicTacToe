dfa3bc593b9fb8f8e45daa986f77be65bb8f93e2
Matthieu Lemaile <mathieu.lemaile@mymoneybank.com>
2020-03-24 21:11:59 +0100

    Improved WinCondition check.

--- src/main/java/fr/lemaile/morpion/controller/Board.java
+++ src/main/java/fr/lemaile/morpion/controller/Board.java
@@ -17,6 +17,8 @@ public class Board implements BoardListener {
     private final Player playerTwo;
     private final BoardUi boardUi;
     private Player currentPlayer;
+    private int moveCount;
+    private boolean won;
 
     public Board(UiFactory uiFactory) {
         playerOne = new Player();
@@ -25,6 +27,8 @@ public class Board implements BoardListener {
         playerTwo.setSymbol(Symbol.NOUGHT);
         currentPlayer = playerOne;
         boxes = new ArrayList<>();
+        moveCount = 0;
+        won = false;
         IntStream.range(0, 9).forEach(i -> boxes.add(new Box(Symbol.EMPTY)));
         boardUi = uiFactory.createBoard(boxes, playerOne, playerTwo);
         boardUi.updateBoard(boxes, currentPlayer);
@@ -32,6 +36,7 @@ public class Board implements BoardListener {
 
     @Override
     public void updateBox(int boxNumber) {
+        moveCount++;
         Player playingPlayer = currentPlayer;
         boxes.get(boxNumber).setSymbol(playingPlayer.getSymbol());
         if (currentPlayer.equals(playerOne)) {
@@ -47,104 +52,112 @@ public class Board implements BoardListener {
         Symbol lastMoveSymbol = evaluatedPlayer.getSymbol();
         switch (lastMoveBoxNumber) {
             case 0:
-                if (boxes.get(1).getSymbol().equals(lastMoveSymbol) && boxes.get(2).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
-                if (boxes.get(3).getSymbol().equals(lastMoveSymbol) && boxes.get(6).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
-                if (boxes.get(4).getSymbol().equals(lastMoveSymbol) && boxes.get(8).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
+                won = checkWinFirstRow(lastMoveSymbol) ||
+                        checkWinFirstColumn(lastMoveSymbol) ||
+                        checkWinDiagonal(lastMoveSymbol);
                 break;
             case 1:
-                if (boxes.get(0).getSymbol().equals(lastMoveSymbol) && boxes.get(2).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
-                if (boxes.get(4).getSymbol().equals(lastMoveSymbol) && boxes.get(7).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
+                won = checkWinFirstRow(lastMoveSymbol) ||
+                        checkWinSecondColumn(lastMoveSymbol);
                 break;
             case 2:
-                if (boxes.get(0).getSymbol().equals(lastMoveSymbol) && boxes.get(1).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
-                if (boxes.get(5).getSymbol().equals(lastMoveSymbol) && boxes.get(8).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
-                if (boxes.get(4).getSymbol().equals(lastMoveSymbol) && boxes.get(6).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
+                won = checkWinFirstRow(lastMoveSymbol) ||
+                        checkWinThirdColumn(lastMoveSymbol) ||
+                        checkWinAntiDiagonal(lastMoveSymbol);
                 break;
             case 3:
-                if (boxes.get(4).getSymbol().equals(lastMoveSymbol) && boxes.get(5).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
-                if (boxes.get(0).getSymbol().equals(lastMoveSymbol) && boxes.get(6).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
+                won = checkWinSecondRow(lastMoveSymbol) ||
+                        checkWinFirstColumn(lastMoveSymbol);
                 break;
             case 4:
-                if (boxes.get(3).getSymbol().equals(lastMoveSymbol) && boxes.get(5).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
-                if (boxes.get(1).getSymbol().equals(lastMoveSymbol) && boxes.get(7).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
-                if (boxes.get(0).getSymbol().equals(lastMoveSymbol) && boxes.get(8).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
-                if (boxes.get(2).getSymbol().equals(lastMoveSymbol) && boxes.get(6).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
+                won = checkWinSecondRow(lastMoveSymbol) ||
+                        checkWinSecondColumn(lastMoveSymbol) ||
+                        checkWinDiagonal(lastMoveSymbol) ||
+                        checkWinAntiDiagonal(lastMoveSymbol);
                 break;
             case 5:
-                if (boxes.get(3).getSymbol().equals(lastMoveSymbol) && boxes.get(4).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
-                if (boxes.get(2).getSymbol().equals(lastMoveSymbol) && boxes.get(8).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
+                won = checkWinSecondRow(lastMoveSymbol) ||
+                        checkWinThirdColumn(lastMoveSymbol);
                 break;
             case 6:
-                if (boxes.get(7).getSymbol().equals(lastMoveSymbol) && boxes.get(8).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
-                if (boxes.get(0).getSymbol().equals(lastMoveSymbol) && boxes.get(3).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
-                if (boxes.get(2).getSymbol().equals(lastMoveSymbol) && boxes.get(4).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
+                won = checkWinThirdRow(lastMoveSymbol) ||
+                        checkWinFirstColumn(lastMoveSymbol) ||
+                        checkWinAntiDiagonal(lastMoveSymbol);
                 break;
             case 7:
-                if (boxes.get(6).getSymbol().equals(lastMoveSymbol) && boxes.get(8).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
-                if (boxes.get(1).getSymbol().equals(lastMoveSymbol) && boxes.get(4).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
+                won = checkWinThirdRow(lastMoveSymbol) ||
+                        checkWinSecondColumn(lastMoveSymbol);
                 break;
             case 8:
-                if (boxes.get(6).getSymbol().equals(lastMoveSymbol) && boxes.get(7).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
-                if (boxes.get(2).getSymbol().equals(lastMoveSymbol) && boxes.get(5).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
-                if (boxes.get(0).getSymbol().equals(lastMoveSymbol) && boxes.get(4).getSymbol().equals(lastMoveSymbol)) {
-                    boardUi.won(evaluatedPlayer);
-                }
+                won = checkWinThirdRow(lastMoveSymbol) ||
+                        checkWinThirdColumn(lastMoveSymbol) ||
+                        checkWinDiagonal(lastMoveSymbol);
                 break;
             default:
-
         }
+        if (won) {
+            boardUi.won(evaluatedPlayer);
+        } else {
+            if (moveCount == 9) {
+                boardUi.pat();
+            }
+        }
+    }
+
+    private boolean checkWinFirstRow(Symbol lastMoveSymbol) {
+        return boxes.get(0).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(1).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(2).getSymbol().equals(lastMoveSymbol);
+    }
+
+    private boolean checkWinSecondRow(Symbol lastMoveSymbol) {
+        return boxes.get(3).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(4).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(5).getSymbol().equals(lastMoveSymbol);
+    }
+
+    private boolean checkWinThirdRow(Symbol lastMoveSymbol) {
+        return boxes.get(6).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(7).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(8).getSymbol().equals(lastMoveSymbol);
+    }
+
+    private boolean checkWinFirstColumn(Symbol lastMoveSymbol) {
+        return boxes.get(0).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(3).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(6).getSymbol().equals(lastMoveSymbol);
+    }
+
+    private boolean checkWinSecondColumn(Symbol lastMoveSymbol) {
+        return boxes.get(1).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(4).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(7).getSymbol().equals(lastMoveSymbol);
+    }
+
+    private boolean checkWinThirdColumn(Symbol lastMoveSymbol) {
+        return boxes.get(2).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(5).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(8).getSymbol().equals(lastMoveSymbol);
+    }
+
+    private boolean checkWinDiagonal(Symbol lastMoveSymbol) {
+        return boxes.get(0).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(4).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(8).getSymbol().equals(lastMoveSymbol);
+    }
+
+    private boolean checkWinAntiDiagonal(Symbol lastMoveSymbol) {
+        return boxes.get(2).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(4).getSymbol().equals(lastMoveSymbol) &&
+                boxes.get(6).getSymbol().equals(lastMoveSymbol);
     }
 
     @Override
     public void newMatch() {
         boxes.forEach(b -> b.setSymbol(Symbol.EMPTY));
-        boardUi.updateBoard(boxes, currentPlayer);
+        moveCount = 0;
+        won = false;
         currentPlayer = playerOne;
+        boardUi.updateBoard(boxes, currentPlayer);
     }
 }
